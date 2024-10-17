0345df43bdb4d93d13718efda63aa4963f5695b6
mgreen20 <mgreen_tk421@yahoo.com>
2024-10-17 11:37:02 -0500

    added jquery

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 //javascript 
-$('#d').click(function(){
-    var filepath = $(this).attr('data-filepath');
-    top.location.href = filepath;
+
+  $(document).ready(function() {
+    $("#dwnResume").click(function(){
+        var fileUrl = '../MGreen_UX_UI_Designer.pdf';
+        var fileName = 'MGreen_UX_UI_Designer.pdf';
+        var link = document.createElement('a');
+        link.href = fileUrl;
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        //alert('download')
+    }); 
 });
